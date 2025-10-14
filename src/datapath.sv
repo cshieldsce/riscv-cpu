99d49665c2e9c1c066400f31b1e4aeae09e9c9a5
Charles <charlz.shields@gmail.com>
2025-10-14 13:37:00 +0100

    feat: Add datapath for r-type instructions

---
+++ src/datapath.sv
@@ -0,0 +1,29 @@
+module DataPath (
+    input logic clk, RegWrite,
+    input  logic [3:0]  ALUControl,
+    input logic[4:0] rs1, rs2, rd,
+);
+
+    logic [31:0] ReadData1, ReadData2;
+    logic [31:0] ALUResult;
+
+    RegFile reg_file_inst (
+        .clk(clk),
+        .RegWrite(RegWrite),
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd(rd),
+        .WriteData(ALUResult), // Write ALU result back to register file
+        .ReadData1(ReadData1),
+        .ReadData2(ReadData2)
+    );
+
+    ALU alu_inst (
+        .A(ReadData1),
+        .B(ReadData2), 
+        .ALUControl(ALUControl),
+        .Result(ALUResult),
+        .Zero() 
+    );
+
+endmodule
