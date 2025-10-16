d4deff2f570a4ac8df68a7fa4b1854b7f76782fe
Charles <charlz.shields@gmail.com>
2025-10-16 19:34:55 +0100

    feat: Implement program counter

---
+++ src/pc.sv
@@ -0,0 +1,15 @@
+module PC (
+    input logic clk, rst,
+    input logic [31:0] pc_in,
+    output logic [31:0] pc_out
+);
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            pc_out <= 32'd0;
+        end else begin
+            pc_out <= pc_in
+        end
+    end
+
+endmodule
