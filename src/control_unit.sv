f3ab22b6a0f802952e0ff5e809b7fe71d19f219d
Charles <charlz.shields@gmail.com>
2025-10-24 12:19:46 +0100

    feat: Implement handling for i-type instructions

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -4,6 +4,7 @@ module ControlUnit (
     input logic [6:0] funct7,
     output logic RegWrite,
     output logic [3:0] ALUControl
+    output logic ALUSrc
 );
 
     parameter OP_ADD = 4'b0010;
@@ -12,10 +13,13 @@ module ControlUnit (
         // Set controls to an idle state (0)
         RegWrite = 1'b0;
         ALUControl = 4'b0000;
+        ALUSrc = 1'b0;
 
         // Since the opcode is shared we check funct3 and funct7 to determine the specific operation
         case (opcode)
-            7'b0110011: begin // Handle R-type instructions
+
+            // Handle R-type instructions
+            7'b0110011: begin 
                 RegWrite = 1'b1;
                 case ({funct7, funct3})
                     {7'b0000000, 3'b000}: ALUControl = OP_ADD; // add
@@ -23,6 +27,16 @@ module ControlUnit (
                     default: ALUControl = 4'b0000;
                 endcase
             end
+
+            // Handle I-type instructions
+            7'b0010011: begin // case for 'addi'
+                RegWrite = 1'b1;
+                ALUSrc = 1'b1;
+                ALUControl = OP_ADD
+            end
+
+            // ldr/str will go here
+            
         endcase
     end
 
