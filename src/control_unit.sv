9c240ee826677c99801139f27b10c3b8a7ecf5da
Charles <charlz.shields@gmail.com>
2025-11-13 18:47:15 +0000

    feat: Add wires for BEQ instruction

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -6,18 +6,21 @@ module ControlUnit (
     output logic [3:0] ALUControl,
     output logic ALUSrc,
     output logic MemWrite,
-    output logic [1:0] MemToReg // Select write-back data
+    output logic [1:0] MemToReg, // Select write-back data
+    output logic Branch
 );
 
     parameter OP_ADD = 4'b0010;
     parameter OP_SUB = 4'b0110;
+
     always_comb begin
-        // Set controls to an idle state (0)
+        // Default control signals
         RegWrite = 1'b0;
         ALUControl = 4'b0000;
         ALUSrc = 1'b0;
         MemWrite = 1'b0;
         MemToReg = 2'b00;
+        Branch = 1'b0;
 
         // Since the opcode is shared we check funct3 and funct7 to determine the specific operation
         case (opcode)
@@ -56,6 +59,13 @@ module ControlUnit (
                 ALUSrc = 1'b1; // Use Immediate for address offset
                 MemWrite = 1'b1; // 'sw' writes to memory
                 ALUControl = OP_ADD; // ALU calculates rs1 + imm
+            end
+
+            // Handle B-type instructions (Branch Equal)
+            7'b1100011: begin
+                Branch = 1'b1; // 'beq' instruction
+                ALUSrc = 1'b0; // Use ReadData2
+                ALUControl = OP_SUB; // ALU performs subtraction for comparison
             end
 
         endcase
