e5b83b9ed784424ac166a90860b06349d4702e98
Charles <charlz.shields@gmail.com>
2025-11-13 19:52:30 +0000

    feat: Add J-type immediate

--- src/imm_gen.sv
+++ src/imm_gen.sv
@@ -7,6 +7,7 @@ module ImmGen (
     logic [31:0] imm_I; // For I-type (addi, lw)
     logic [31:0] imm_S; // For S-type (sw)
     logic [31:0] imm_B; // For B-type (beq)
+    logic [31:0] imm_J; // For J-type (jal)
 
     // I-type Immediate 
     // Immediate is bits [31:20]. Sign-extend from bit [31].
@@ -24,6 +25,15 @@ module ImmGen (
                     instruction[30:25], 
                     instruction[11:8], 1'b0};
 
+    // J-Type Immediate (JAL)
+    // Immediate is split: [31], [19:12], [20], [30:21], with a 0 as LSB
+    assign imm_J = {{11{instruction[31]}},
+                    instruction[31],
+                    instruction[19:12],
+                    instruction[20],
+                    instruction[30:21],
+                    1'b0};
+
     // Output mux based on opcode
     always_comb begin
         case (opcode)
@@ -35,6 +45,8 @@ module ImmGen (
                 imm_out = imm_S;
             7'b1100011: // B-type (beq)
                 imm_out = imm_B;
+            7'b1101111: // J-type (jal)
+                imm_out = imm_J;
             default:
                 imm_out = 32'd0; // Default case
         endcase
