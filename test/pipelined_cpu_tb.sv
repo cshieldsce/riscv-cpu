87c89fc315f70f8365bdac96a8af997f89431e81
Charles <charlz.shields@gmail.com>
2025-12-03 12:36:48 +0000

    test: Update testbench to use the forwarding unit

--- test/pipelined_cpu_tb.sv
+++ test/pipelined_cpu_tb.sv
@@ -23,7 +23,7 @@ module pipelined_cpu_tb;
         // --- SETUP ---
         // Initialize registers directly
         cpu_inst.reg_file_inst.register_memory[1] = 32'd0; 
-        cpu_inst.reg_file_inst.register_memory[2] = 32'd0;
+        //cpu_inst.reg_file_inst.register_memory[2] = 32'd0;
 
         // --- RESET ---
         rst = 1;
@@ -36,13 +36,22 @@ module pipelined_cpu_tb;
         $display("Running for 10 cycles...");
         repeat (10) @(posedge clk);
         #1;
+
+        $display("Register x1: %d (Expected 10)", cpu_inst.reg_file_inst.register_memory[1]);
+        $display("Register x2: %d (Expected 20)", cpu_inst.reg_file_inst.register_memory[2]);
+        
+        if (cpu_inst.reg_file_inst.register_memory[2] == 32'd20)
+            $display("PASS: Data Hazard handled correctly!");
+        else
+            $display("FAIL: Data Hazard detected! Read old value.");
         
         // --- VERIFY ---
         // We will check the results manually in the simulation output/waveform for now.
-        $display("Final PC: %h", cpu_inst.if_pc);
-        $display("Register x1: %d (Expected 10)", cpu_inst.reg_file_inst.register_memory[1]);
-        $display("Register x2: %d (Expected 20)", cpu_inst.reg_file_inst.register_memory[2]);
-        $display("Register x3: %d (Expected 30)", cpu_inst.reg_file_inst.register_memory[3]);
+        // $display("Final PC: %h", cpu_inst.if_pc);
+        // $display("Register x1: %d (Expected 10)", cpu_inst.reg_file_inst.register_memory[1]);
+        // $display("Register x2: %d (Expected 20)", cpu_inst.reg_file_inst.register_memory[2]);
+        // $display("Register x3: %d (Expected 30)", cpu_inst.reg_file_inst.register_memory[3]);
+
 
         $display("Testbench Finished.");
         $finish;
