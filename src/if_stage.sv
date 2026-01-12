f70b611d0ffe382c372994e831a736acc0c9321f
cshieldsce <cshieldsce@gmail.com>
2026-01-11 18:14:19 -0700

    feat: Remove the internal instruction memory

--- src/if_stage.sv
+++ src/if_stage.sv
@@ -1,12 +1,12 @@
 module IF_Stage (
     input logic clk, rst,
     input logic [31:0] next_pc_in,
+    input logic [31:0] instruction_in,  // External instruction
     output logic [31:0] instruction_out,
     output logic [31:0] pc_out,
     output logic [31:0] pc_plus_4_out
 );
 
-    logic [31:0] next_pc;
     PC pc_inst (
         .clk(clk),
         .rst(rst),
@@ -14,12 +14,9 @@ module IF_Stage (
         .pc_out(pc_out)
     );
 
-    InstructionMemory imem_inst (
-        .Address(pc_out),
-        .Instruction(instruction_out)
-    );
+    // Instruction now comes from external memory
+    assign instruction_out = instruction_in;
 
-    // Add PC + 4
     assign pc_plus_4_out = pc_out + 32'd4;
 
 endmodule
