283ab891db8b772c7b2806f3977d3731f0f1f3bd
cshieldsce <cshieldsce@gmail.com>
2026-01-12 19:56:46 -0700

    feat: Add write-back MUX for RegFile

--- src/datapath.sv
+++ src/datapath.sv
@@ -1,11 +1,25 @@
 module DataPath (
     input logic clk, RegWrite,
     input  logic [3:0]  ALUControl,
-    input logic[4:0] rs1, rs2, rd
+    input logic[4:0] rs1, rs2, rd,
+    input logic [31:0] PC,         
+    input logic [1:0]  MemToReg,   
+    input logic [31:0] ReadDataMem,
+    output logic [31:0] ALUResult,
+    output logic [31:0] WriteBackData
 );
 
     logic [31:0] ReadData1, ReadData2;
-    logic [31:0] ALUResult;
+
+    // --- WRITEBACK MUX ---
+    always_comb begin
+        case (MemToReg)
+            2'b00: WriteBackData = ALUResult;       // R-Type, I-Type
+            2'b01: WriteBackData = ReadDataMem;     // Load (LW)
+            2'b10: WriteBackData = PC + 4;          // JAL / JALR (Return Address)
+            default: WriteBackData = 32'b0;
+        endcase
+    end
 
     RegFile reg_file_inst (
         .clk(clk),
@@ -13,7 +27,7 @@ module DataPath (
         .rs1(rs1),
         .rs2(rs2),
         .rd(rd),
-        .WriteData(ALUResult), // Write ALU result back to register file
+        .WriteData(WriteBackData), // Write selected result back to register file
         .ReadData1(ReadData1),
         .ReadData2(ReadData2)
     );
